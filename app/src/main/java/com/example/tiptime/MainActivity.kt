140f24a0ec6699c30c4b4cd5b99ff0522871d372
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-01-05 13:54:00 +0700

    Calculate the tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
